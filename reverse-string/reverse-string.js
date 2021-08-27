adf0c37241590840652265fe0d690b753d7ac4a1
babafemij-k <olaolujkb21@gmail.com>
2021-08-27 04:30:51 +0100

    Side Exercise Two

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const reverseString = (str) => {
+  let newString = "";
+
+  // Decrementing for loop
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+
+  return newString;
+};
