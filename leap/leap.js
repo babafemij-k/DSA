c2e9eaab3510f81faf8c6e38120dc0810cf96e1b
babafemij-k <olaolujkb21@gmail.com>
2021-10-16 23:47:20 +0100

    Modified isLeap function.

--- leap/leap.js
+++ leap/leap.js
@@ -6,9 +6,8 @@
 // Year must be divisible by 4 and not divisible by 100
 // If year is divisible by 4 and 100, then year must also be divisible by 400
 export const isLeap = (year) => {
-  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-    return true;
-  } else {
-    return false;
-  }
+  const isLeap =
+    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? true : false;
+
+  return isLeap;
 };
